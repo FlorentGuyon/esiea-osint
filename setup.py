04ce43d45828d88aaba113bdec83869bc7097b3e
FlorentGuyon <florent.guyon@protonmail.com>
2019-12-04 11:58:28 +0100

    New requirements downloading program

--- setup.py
+++ setup.py
@@ -3,42 +3,76 @@
 
 ### IMPORTS -----------------------------------------------------------------------------------------------------------------------------------------
 
-# Manipulate system paths
-import os.path
+import os, sys, subprocess, math
 
-# Read system informations
-import sys
-
-# Specific functions for this script
-from libs.functions import loadModules, fatalError, isFile, warning, success
+# Check the current version of Python
+if sys.version_info[0] < 3:
+    print("Please, use Python 3.")
+    # Quit the program
+    exit()
 
 ### MAIN --------------------------------------------------------------------------------------------------------------------------------------------
 
-# Load the modules configuration file
-modules = loadModules()
+# Absolut path to the modules directory
+modulesPath = os.sep.join([os.path.dirname(os.path.abspath(__file__)), "modules"])
+# List of modules
+modulesList = os.listdir(modulesPath)
 
-# Empty the requirements list
-requirements = ""
-# For all module in the configuration file
-for index, (moduleKey, moduleData) in enumerate(modules.items()):
-	# Set the requirements path of the current module
-	requirementsPath = os.sep.join([os.path.abspath("."), "modules", moduleKey, "requirements.txt"])
-	# If the requirements file exists
-	if isFile(requirementsPath):
-		# Open the file
-		with open(requirementsPath, "r") as file:
-			# extract the data		
-			requirements += file.read() + "\n"
-	# If the requirements file does not exist
-	else:
-		# Print a warning message
-		warning("Requirements file not found at " + requirementsPath + ".")
+# If there at least one module
+if len(modulesList) > 0:
+	# Print the banner
+	print(" Download of requirements:\n")
+	# Lenght of the longest module name for esthetical printing purpose
+	longestModuleName = max(modulesList, key=len)
 
-# Set the path of the total requirements file
-finalRequirementsPath = os.sep.join([os.path.abspath("."), "requirements.txt"])
-# Open the file
-with open(finalRequirementsPath, "w") as file:
-	# Write the list of requirements
-	file.write(requirements)
-	# Print advices about the downloading of the requirements
-	success("The list of requirements is now available at \"" + finalRequirementsPath + "\".\nMake sure to download them before running the \"main.py\" script.\nYou can download the requirements by running the following command: \"python3 -m pip install --upgrade -r " + finalRequirementsPath + "\".")
+	# For each module in the list
+	for module in modulesList:
+		# Set the path to the requirements file
+		filePath = os.sep.join((modulesPath, module, "requirements.txt"))
+		# If a requirements file exist
+		if os.path.exists(filePath):
+			# Print the current module name
+			print("\t■ {}".format(module) + " " * (len(longestModuleName) - len(module)), end="\t\t")
+			
+			# Open the requirements file
+			with open(filePath, "r") as file:
+				# Set the list of packages
+				packages = file.read().split("\n")
+				# Remove the empty names
+				packages = [package for package in packages if package != ""]
+				# Set the count of package for printing purpose
+				packagesCount = len(packages)
+				# Set a current package index for printing purpose
+				currentPackageIndex = 0
+
+				# For each package in the list
+				for package in packages:
+					# Start the download
+					output = subprocess.Popen([sys.executable, "-m", "pip", "install", package], stdout=open(os.devnull, "wb"), stderr=subprocess.PIPE)
+					# Get the errors
+					error = output.communicate()[1].decode()
+					# If there is at least one error
+					if error != "":
+						# Print them
+						print("\n\n{}".format(error))
+					# If there is no error
+					else:
+						# Increase the current package index for printing purpose
+						currentPackageIndex += 1
+						# Set the current download progress for printing purpose
+						currentProgress = int(currentPackageIndex * 100 / packagesCount)
+						# If these is not the first package of the list
+						if (currentPackageIndex > 1):
+							# Erase the previous progress
+							sys.stdout.write("\b" * 20)
+							sys.stdout.flush()
+						# Print the new progress
+						print("{}%\t[".format(currentProgress) + ("■" * int(currentProgress / 10)) + (" " * math.ceil((100 - currentProgress) / 10)) + "]{}".format("\n" if packagesCount == currentPackageIndex else ""), end="")
+		
+		# If no requirements file found
+		else:
+			print("No requirements found for the {} module".format(module))
+
+# If no modules fond
+else:
+	print("No modules found at {}.".format(modulesPath))
